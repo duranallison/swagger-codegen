09bc8edebaa903d0e42650ecb6f99bb78f92ce1a
Maxim Antonov <mantonov@gmail.com>
2021-08-24 17:33:30 +0700

    PD-576 Use generated topo API instead of topo.service in frontend - deployment script for maven plugin - version & groupId fix

--- deploy_codegen_cli.sh
+++ deploy_codegen_cli.sh
@@ -4,5 +4,12 @@ mvn -s ~/.m2/settings.xml.deploy -P deployment deploy:deploy-file -DgroupId="com
   -Dpackaging="jar" \
   -Dfile="modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
 
+mvn -s ~/.m2/settings.xml.deploy -P deployment deploy:deploy-file -DgroupId="com.rightontrek" \
+  -DartifactId="swagger-codegen-maven-plugin" \
+  -Dversion="2.4.21-SNAPSHOT" \
+  -Dpackaging="jar" \
+  -Dfile="modules/swagger-codegen-maven-plugin/target/swagger-codegen-maven-plugin-2.4.21-SNAPSHOT.jar"
+
+
 #  -DrepositoryId="rightontrek-nexus" \
 #  -Durl="http://cicd.rightontrek.com/nexus/content/repositories/snapshots"
